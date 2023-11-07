9c6c8b062324817dd3720095b5f82227ba76ec08
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-11-07 11:38:29 +0200

    string

--- js/for.js
+++ js/for.js
@@ -15,6 +15,25 @@ for (let i = 1; i <= 60; i++) {
   // console.log(`<li>sekunde ${i}</li>`);
   ilgasString = ilgasString + `<li>sekunde ${i}</li>\n`;
 }
-console.log('ilgasString ===', ilgasString);
+// console.log('ilgasString ===', ilgasString);
 
 // su for ciklu atspausdinti skaicius nuo 25 iki 80 kas antra skaiciu
+
+for (let i = 25; i < 81; i += 2) {
+  // console.log('i ===', i);
+}
+
+// atspausdinti visus 3 kartotinius nuo 1 iki 100
+// sukti cikla nuo 1 iki 100
+for (let i = 1; i <= 100; i++) {
+  //// tikrtinti ar skaicius yra 3 kartotinis?
+  if (i % 3 === 0) {
+    console.log('kartotinis ===', i);
+  }
+}
+
+// psiaudo kodas
+// sukti cikla nuo 1 iki 100
+//// tikrtinti ar skaicius yra 3 kartotinis?
+//// taip - spausdinam
+//// ne - praleidziam
---
+++ js/string.js
@@ -0,0 +1,11 @@
+'use strict';
+console.log('string.js file was loaded');
+
+//         0123456789
+let str = 'Laba diena';
+
+console.log('str ===', str);
+
+// prima raide
+let primRaide = str.charAt(0);
+console.log('primRaide ===', primRaide);
